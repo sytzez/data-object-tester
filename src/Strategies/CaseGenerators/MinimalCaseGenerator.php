7df357c469af0f7aa15c1313083670c861da5e8c
sytzez <sytzez@gmail.com>
2021-10-15 02:07:13 +0200

    [Feature] Implement minimal case generator

--- src/Strategies/CaseGenerators/MinimalCaseGenerator.php
+++ src/Strategies/CaseGenerators/MinimalCaseGenerator.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Sytzez\DataObjectTester\Strategies\CaseGenerators;
 
+use Sytzez\DataObjectTester\Builders\ObjectCaseBuilder;
 use Sytzez\DataObjectTester\Contracts\Strategies\CaseGeneratorStrategy;
 use Sytzez\DataObjectTester\DataObjects\ClassDescription;
+use Sytzez\DataObjectTester\DataObjects\InputOutputPair;
 use Sytzez\DataObjectTester\DataObjects\ObjectCase;
+use Sytzez\DataObjectTester\DataObjects\PropertyCase;
+use Sytzez\DataObjectTester\DataObjects\PropertyDescription;
 
 final class MinimalCaseGenerator implements CaseGeneratorStrategy
 {
@@ -16,6 +20,55 @@ final class MinimalCaseGenerator implements CaseGeneratorStrategy
      */
     public function generate(ClassDescription $classDescription): iterable
     {
-        return [];
+        foreach ($classDescription->getPropertyDescriptions() as $propertyDescription) {
+            yield from $this->generateCasesForProperty($classDescription, $propertyDescription);
+        }
+    }
+
+    /**
+     * @param ClassDescription $classDescription
+     * @param PropertyDescription $propertyDescription
+     * @return iterable<ObjectCase>
+     */
+    private function generateCasesForProperty(
+        ClassDescription $classDescription, PropertyDescription $propertyDescription
+    ): iterable {
+
+        foreach ($propertyDescription->getInputOutputPairs() as $inputOutputPair) {
+            yield $this->createObjectCaseWithIOPair(
+                $classDescription,
+                $propertyDescription,
+                $inputOutputPair,
+            );
+        }
+    }
+
+    private function createObjectCaseWithIOPair(
+        ClassDescription $classDescription,
+        PropertyDescription $selectedPropertyDescription,
+        InputOutputPair $inputOutputPair,
+    ): ObjectCase {
+
+        $builder = new ObjectCaseBuilder();
+
+        foreach ($classDescription->getPropertyDescriptions() as $propertyDescription) {
+            if ($propertyDescription === $selectedPropertyDescription) {
+                $builder->addPropertyCase(
+                    new PropertyCase(
+                        $propertyDescription,
+                        $inputOutputPair,
+                    )
+                );
+            } else {
+                $builder->addPropertyCase(
+                    new PropertyCase(
+                        $propertyDescription,
+                        $propertyDescription->getInputOutputPairs()[0],
+                    )
+                );
+            }
+        }
+
+        return $builder->getResult();
     }
 }
