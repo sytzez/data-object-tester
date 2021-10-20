80295315479636cf066848b18093d6d150a38892
sytzez <sytzez@gmail.com>
2021-10-20 22:56:25 +0200

    [Fix] Add check on trait to see if class extends TestCase

--- src/TestsDataObjects.php
+++ src/TestsDataObjects.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Sytzez\DataObjectTester;
 
+use DomainException;
+use PHPUnit\Framework\TestCase;
 use Sytzez\DataObjectTester\Contracts\Generators\CaseGeneratorStrategy;
 use Sytzez\DataObjectTester\DataObjects\ClassExpectation;
 
@@ -13,6 +15,10 @@ trait TestsDataObjects
         ClassExpectation $classExpectation,
         ?CaseGeneratorStrategy $caseGenerator = null,
     ): void {
+        if (! $this instanceof TestCase) {
+            throw new DomainException('This trait must be used on an instance of ' . TestCase::class);
+        }
+
         $tester = new DataObjectTester($this, $classExpectation, $caseGenerator);
 
         $tester->run();
