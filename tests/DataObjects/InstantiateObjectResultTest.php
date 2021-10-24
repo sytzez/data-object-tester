72543924726ac138a126a5ff7493c136611ca83d
sytzez <sytzez@gmail.com>
2021-10-24 19:11:24 +0200

    [Test] Data objecttest for InstantiateObjectResult class

---
+++ tests/DataObjects/InstantiateObjectResultTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sytzez\DataObjectTester\Tests\DataObjects;
+
+use Sytzez\DataObjectTester\DataObjects\ClassExpectation;
+use Sytzez\DataObjectTester\DataObjects\InstantiateObjectResult;
+use Sytzez\DataObjectTester\DataObjectTestCase;
+use Sytzez\DataObjectTester\Generators\MaximalCaseGenerator;
+use Sytzez\DataObjectTester\PropertyCases\DefaultPropertyCase;
+use Sytzez\DataObjectTester\Tests\TestHelpers\DataClass;
+use Sytzez\DataObjectTester\Tests\TestHelpers\EmptyClass;
+
+class InstantiateObjectResultTest extends DataObjectTestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_the_right_values(): void
+    {
+        $this->testDataObjects(
+            ClassExpectation::create(InstantiateObjectResult::class, [
+                'getObject' => [
+                    null,
+                    new EmptyClass(),
+                    new DataClass('a', 1, []),
+                ],
+                'exceptionWasCaught' => [
+                    true,
+                    false,
+                    new DefaultPropertyCase(false),
+                ],
+            ]),
+            new MaximalCaseGenerator(),
+        );
+    }
+}
